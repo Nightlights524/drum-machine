6e0186b0c1770dff1f916279d966f8a3e10ed258
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-10-15 00:16:33 -0500

    Add drum pads

--- src/index.js
+++ src/index.js
@@ -16,7 +16,26 @@ import './index.css';
 //   );
 // }
 
-class App extends React.Component {
+function DrumPad(props) {
+  return (
+    <button
+      className="drum-pad"
+      id={props.clipID}
+      onMouseDown={() => {document.getElementById(props.keyTrigger).play()}}
+    >
+      {props.keyTrigger}
+      <audio
+        id={props.keyTrigger}
+        className="clip"
+        src={props.clipSource}
+      >
+        Your browser does not support the <code>audio</code> element.
+      </audio>
+    </button>
+  );
+}
+
+class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -26,8 +45,29 @@ class App extends React.Component {
   
   render() {  
     return (
-      <div>
-        <h1>App Title</h1>
+      <div id="drum-machine">
+        <h1 id="title">Drum Machine</h1>
+        <h2 id="display">HIT A BUTTON</h2>
+        <div id="pad-section">
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="Q" clipID={"Kick"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="W" clipID={"808"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="E" clipID={"Snare1"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="A" clipID={"Snare2"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="S" clipID={"Clap"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="D" clipID={"Hi-Hat"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="Z" clipID={"Crash1"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="X" clipID={"Crash2"} />
+          <DrumPad clipSource="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+            keyTrigger="C" clipID={"Ride"} />
+        </div>
       </div>
     );
   }
@@ -36,6 +76,6 @@ class App extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <App />,
+  <DrumMachine />,
   document.getElementById('root')
 );
